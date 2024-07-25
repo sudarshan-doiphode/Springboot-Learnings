557752f5d9ff88bc69f9b9290fd18fc0968ace45
sudarshan-doiphode <doiphode.sudarshan3@gmail.com>
2024-07-25 20:41:10 +0530

    Containerize springboot app with Paketo Buildpacks 🤯

---
+++ docker-image/stocks-paketo-buildpacks/src/main/java/com/darsh/StocksPaketoBuildpacksApplication.java
@@ -0,0 +1,13 @@
+package com.darsh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StocksPaketoBuildpacksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StocksPaketoBuildpacksApplication.class, args);
+	}
+
+}
